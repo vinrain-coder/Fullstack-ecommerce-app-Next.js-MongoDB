59304126b43fb44448e7e0b6fc29b83d6b5e0407
vinrain-coder <vincentombogo57@gmail.com>
2025-01-10 15:40:47 +0000

    04-connect to mongodb database

---
+++ lib/db/seed.ts
@@ -0,0 +1,27 @@
+import { connectToDatabase } from ".";
+import data from "@/lib/data";
+import Product from "./models/product.model";
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+
+loadEnvConfig(cwd());
+
+const main = async () => {
+  try {
+    const { products } = data;
+    await connectToDatabase(process.env.MONGODB_URI);
+
+    await Product.deleteMany();
+    const createdProducts = await Product.insertMany(products);
+
+    console.log({
+      createdProducts,
+      message: "Seeded database successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed database");
+  }
+};
+
+main();
