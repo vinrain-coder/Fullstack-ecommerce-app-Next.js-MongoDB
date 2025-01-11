ff8f31f2a625fc49a1769a8fc87aa38220029414
vinrain-coder <vincentombogo57@gmail.com>
2025-01-11 09:14:12 +0000

    06-add todays deals slider

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { connectToDatabase } from "../db";
-import Product from "../db/models/product.model";
+import Product, { IProduct } from "../db/models/product.model";
 
 export async function getAllCategories() {
   await connectToDatabase();
@@ -34,4 +34,22 @@ export async function getProductsForCard({
     href: string;
     image: string;
   }[];
+}
+
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+  tag,
+  limit = 10,
+}: {
+  tag: string;
+  limit?: number;
+}) {
+  await connectToDatabase();
+  const products = await Product.find({
+    tags: { $in: [tag] },
+    isPublished: true,
+  })
+    .sort({ createdAt: "desc" })
+    .limit(limit);
+  return JSON.parse(JSON.stringify(products)) as IProduct[];
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,21 +1,35 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const formatNumberWithDecimal = (num: number): string => {
-  const [int, decimal] = num.toString().split('.')
-  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int
-}
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : int;
+};
 // PROMPT: [ChatGTP] create toSlug ts arrow function that convert text to lowercase, remove non-word,
 // non-whitespace, non-hyphen characters, replace whitespace, trim leading hyphens and trim trailing hyphens
 
 export const toSlug = (text: string): string =>
   text
     .toLowerCase()
-    .replace(/[^\w\s-]+/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/^-+|-+$/g, '')
-    .replace(/-+/g, '-')
+    .replace(/[^\w\s-]+/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/^-+|-+$/g, "")
+    .replace(/-+/g, "-");
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "KES",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
