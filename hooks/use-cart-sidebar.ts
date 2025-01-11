fb393269154bcddb3369f8322acdd76dd999556a
vinrain-coder <vincentombogo57@gmail.com>
2025-01-11 21:16:35 +0000

    12-create cart sidebar

---
+++ hooks/use-cart-sidebar.ts
@@ -0,0 +1,22 @@
+import { usePathname } from "next/navigation";
+import useDeviceType from "./use-device-type";
+import useCartStore from "./use-cart-store";
+
+const isNotInPaths = (s: string) => {
+  const pathsPattern = `^(?:)?(?:/$|/cart$|/checkout$|/sign-in$|/sign-up$|/order(?:/.*)?$|/account(?:/.*)?$|/admin(?:/.*)?$)?$`;
+  return !new RegExp(pathsPattern).test(s);
+};
+
+function useCartSidebar() {
+  const {
+    cart: { items },
+  } = useCartStore();
+  const deviceType = useDeviceType();
+  const currentPath = usePathname();
+
+  return (
+    items.length > 0 && deviceType === "desktop" && isNotInPaths(currentPath)
+  );
+}
+
+export default useCartSidebar;
