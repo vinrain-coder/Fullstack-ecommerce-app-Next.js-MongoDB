4160486ca44f69f7e7eb95c940440155ccea1e0e
vinrain-coder <vincentombogo57@gmail.com>
2025-01-11 11:00:49 +0000

    08-create product details page

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,5 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "An ecommerce website built with Next.js and MomgoDB";
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
