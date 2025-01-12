7beebec8a19b6af8250628a185b13388f672891a
vinrain-coder <vincentombogo57@gmail.com>
2025-01-12 15:15:39 +0000

    19-fix database connection

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,25 +1,38 @@
 import mongoose from "mongoose";
 
+// Use a global variable for caching the connection
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const cached = (global as any).mongoose || { conn: null, promise: null };
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export const connectToDatabase = async (
   MONGODB_URI = process.env.MONGODB_URI
 ) => {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    // Return the existing connection
+    return cached.conn;
+  }
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
 
-  // Add options for Mongoose connection
-  const options = {
-    retryWrites: true, // Explicitly set this to true or false
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
+  if (!cached.promise) {
+    // Add options for Mongoose connection
+    const options = {
+      retryWrites: true, // Explicitly set to true or false
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, options);
+    // Initialize the connection promise
+    cached.promise = mongoose.connect(MONGODB_URI, options);
+  }
 
+  // Wait for the connection to be established
   cached.conn = await cached.promise;
-
   return cached.conn;
 };
