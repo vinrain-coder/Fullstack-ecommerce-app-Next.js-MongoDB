6002c28e7635f08b4774671af83484164cd9d34d
vinrain-coder <vincentombogo57@gmail.com>
2025-01-13 19:08:46 +0000

    26-add search page

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,29 +1,29 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { getAllCategories } from '@/lib/actions/product.actions'
-import Menu from './menu'
-import Search from './search'
-import data from '@/lib/data'
-import Sidebar from './sidebar'
-import { APP_NAME } from '@/lib/constants'
+import Image from "next/image";
+import Link from "next/link";
+import { getAllCategories } from "@/lib/actions/product.actions";
+import Menu from "./menu";
+import Search from "./search";
+import data from "@/lib/data";
+import Sidebar from "./sidebar";
+import { APP_NAME } from "@/lib/constants";
 // import { getSetting } from '@/lib/actions/setting.actions'
 // import { getTranslations } from 'next-intl/server'
 
 export default async function Header() {
-  const categories = await getAllCategories()
+  const categories = await getAllCategories();
   // const { site } = await getSetting()
   // const t = await getTranslations()
   return (
-    <header className='bg-black  text-white'>
-      <div className='px-2'>
-        <div className='flex items-center justify-between'>
-          <div className='flex items-center'>
+    <header className="bg-black  text-white">
+      <div className="px-2">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
             <Link
-              href='/'
-              className='flex items-center header-button font-extrabold text-2xl m-1 '
+              href="/"
+              className="flex items-center header-button font-extrabold text-2xl m-1 "
             >
               <Image
-                src='/icons/logo.svg'
+                src="/icons/logo.svg"
                 width={40}
                 height={40}
                 alt={`${APP_NAME} logo`}
@@ -32,29 +32,29 @@ export default async function Header() {
             </Link>
           </div>
 
-          <div className='hidden md:block flex-1 max-w-xl'>
+          <div className="hidden md:block flex-1 max-w-xl">
             <Search />
           </div>
           <Menu />
         </div>
-        <div className='md:hidden block py-2'>
+        <div className="md:hidden block py-2">
           <Search />
         </div>
       </div>
-      <div className='flex items-center px-3 mb-[1px]  bg-gray-800'>
+      <div className="flex items-center px-3 mb-[1px]  bg-gray-800">
         <Sidebar categories={categories} />
-        <div className='flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]'>
+        <div className="flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]">
           {data.headerMenus.map((menu) => (
             <Link
               href={menu.href}
               key={menu.href}
-              className='header-button !p-2 '
+              className="header-button !p-2 "
             >
-              {('Header.' + menu.name)}
+              {menu.name}
             </Link>
           ))}
         </div>
       </div>
     </header>
-  )
+  );
 }
--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -1,6 +1,7 @@
 import { SearchIcon } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
+import { getAllCategories } from "@/lib/actions/product.actions";
 
 import {
   Select,
@@ -9,11 +10,16 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../../ui/select";
-
-import { APP_NAME } from "@/lib/constants";
-const categories = ["men", "women", "kids", "accessories"];
+// import { getSetting } from '@/lib/actions/setting.actions'
+// import { getTranslations } from 'next-intl/server'
 
 export default async function Search() {
+  // const {
+  //   site: { name },
+  // } = await getSetting()
+  const categories = await getAllCategories();
+
+  // const t = await getTranslations()
   return (
     <form action="/search" method="GET" className="flex  items-stretch h-10 ">
       <Select name="category">
@@ -31,7 +37,7 @@ export default async function Search() {
       </Select>
       <Input
         className="flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full"
-        placeholder={`Search Site ${APP_NAME}`}
+        placeholder="Search..."
         name="q"
         type="search"
       />
