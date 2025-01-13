84a3b507d687e44986295f52e2d8121b5c52c4de
vinrain-coder <vincentombogo57@gmail.com>
2025-01-13 19:36:45 +0000

    27-add theme color

---
+++ app/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+// import { useTranslations } from 'next-intl'
+
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  //   const t = useTranslations()
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <div className="p-6 rounded-lg shadow-md w-1/3 text-center">
+        <h1 className="text-3xl font-bold mb-4">Error</h1>
+        <p className="text-destructive">{error.message}</p>
+        <Button variant="outline" className="mt-4" onClick={() => reset()}>
+          Try again
+        </Button>
+        <Button
+          variant="outline"
+          className="mt-4 ml-2"
+          onClick={() => (window.location.href = "/")}
+        >
+          Back to home
+        </Button>
+      </div>
+    </div>
+  );
+}
