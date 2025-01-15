9eba3fbdd7ac4d073bb195b785aac79221f47fa9
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-01-15 20:06:26 +0300

    Update i18n-config.ts

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,11 +1,12 @@
 export const i18n = {
   locales: [
-    { code: "en-US", name: "English", icon: "🇺🇸" },
+    { code: "en-US", name: "English (US)", icon: "🇺🇸" },
+    { code: "en-KE", name: "English (Kenya)", icon: "🇰🇪" }, // Added English (Kenya)
     { code: "fr", name: "Français", icon: "🇫🇷" },
-    { code: "sw", name: "Swahili", icon: "sw" },
+    { code: "sw", name: "Swahili", icon: "🇰🇪" },
     { code: "ar", name: "العربية", icon: "🇸🇦" },
   ],
-  defaultLocale: "en-KE",
+  defaultLocale: "en-KE", // Set English (Kenya) as the default locale
 };
 
 export const getDirection = (locale: string) => {
@@ -13,3 +14,4 @@ export const getDirection = (locale: string) => {
 };
 export type I18nConfig = typeof i18n;
 export type Locale = I18nConfig["locales"][number];
+     
