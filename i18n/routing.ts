50cc27cb51803925281448fc90cdf840073d93ae
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-01-15 19:36:05 +0300

    Update routing.ts

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: i18n.locales.map((locale) => locale.code),
-  defaultLocale: "en-US",
+  defaultLocale: "en-KE",
   localePrefix: "as-needed",
   pathnames: {
     // If all locales use the same pathname, a single
