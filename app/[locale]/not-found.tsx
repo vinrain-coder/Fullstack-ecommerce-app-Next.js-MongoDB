82c51b6d707c1d73b49472d215df9a37e8e432a2
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-01-27 19:17:59 +0300

    Update not-found.tsx

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,22 +1,8 @@
-"use client";
-import React from "react";
-
-import { Button } from "@/components/ui/button";
-
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
-      <div className="p-6 rounded-lg shadow-md w-1/3 text-center">
-        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
-        <p className="text-destructive">Could not find requested resource</p>
-        <Button
-          variant="outline"
-          className="mt-4 ml-2"
-          onClick={() => (window.location.href = "/")}
-        >
-          Back to home
-        </Button>
-      </div>
-    </div>
+    <main className="mx-auto max-w-7xl space-y-5 px-5 py-10 text-center">
+      <h1 className="text-3xl font-bold">Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </main>
   );
 }
