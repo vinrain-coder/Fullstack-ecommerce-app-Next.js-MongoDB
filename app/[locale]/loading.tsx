8a610fbc6be266fded15ec1257d0c5c754a2043d
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-02-17 10:50:55 +0000

    fix loading and violet color

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,11 +1,13 @@
+import { Loader2 } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 
 export default async function LoadingPage() {
   const t = await getTranslations();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
-      <div className="p-6 rounded-lg shadow-md w-1/3 text-center">
-        {t("Loading.Loading")}
+      <div className="p-6 rounded-lg shadow-md w-1/3 items-center justify-center flex gap-2">
+        <Loader2 className="animate-spin" />
+        <span>{t("Loading.Loading")}</span>
       </div>
     </div>
   );
