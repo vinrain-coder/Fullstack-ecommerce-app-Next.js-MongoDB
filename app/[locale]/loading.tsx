5bde0b09f546e600c60dff8b989bd0d7b26fe2ed
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-02-23 12:53:04 +0300

    Update loading.tsx

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -5,9 +5,9 @@ export default async function LoadingPage() {
   const t = await getTranslations();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
-      <div className="p-6 rounded-lg shadow-md w-1/3 items-center justify-center flex gap-2">
+      <div className="p-6 rounded-lg shadow-md w-1/3 items-center justify-center flex gap-2 animate-pulse">
         <Loader2 className="animate-spin" />
-        <span className="animate-bounce">{t("Loading.Loading")}</span>
+        <span>{t("Loading.Loading")}</span>
       </div>
     </div>
   );
