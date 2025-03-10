7916b2fd04f03194d57bb59bc3e5482eef21a664
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-03-10 21:13:01 +0000

    Edit product card

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,8 +1,7 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+
 import {
   Card,
   CardContent,
@@ -10,13 +9,12 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import { IProduct } from "@/lib/db/models/product.model";
+
 import Rating from "./rating";
 import { formatNumber, generateId, round2 } from "@/lib/utils";
 import ProductPrice from "./product-price";
 import ImageHover from "./image-hover";
 import AddToCart from "./add-to-cart";
-// import WishlistIcon from "./wishlist-icon";
-// import { useWishlist } from "@/hooks/use-wishlist";
 
 const ProductCard = ({
   product,
@@ -30,8 +28,8 @@ const ProductCard = ({
   hideAddToCart?: boolean;
 }) => {
   const ProductImage = () => (
-    <div className="relative h-52">
-      <Link href={`/product/${product.slug}`}>
+    <Link href={`/product/${product.slug}`}>
+      <div className="relative h-52">
         {product.images.length > 1 ? (
           <ImageHover
             src={product.images[0]}
@@ -49,22 +47,20 @@ const ProductCard = ({
             />
           </div>
         )}
-      </Link>
-      <div
-        className="absolute top-1 right-1"
-        onClick={(e) => e.stopPropagation()} // Stop event bubbling
-      >
-        {/* <WishlistIcon productId={product._id} /> */}
       </div>
-    </div>
+    </Link>
   );
-
   const ProductDetails = () => (
     <div className="flex-1 space-y-2">
       <p className="font-bold">{product.brand}</p>
       <Link
         href={`/product/${product.slug}`}
         className="overflow-hidden text-ellipsis"
+        style={{
+          display: "-webkit-box",
+          WebkitLineClamp: 2,
+          WebkitBoxOrient: "vertical",
+        }}
       >
         {product.name}
       </Link>
@@ -72,6 +68,7 @@ const ProductCard = ({
         <Rating rating={product.avgRating} />
         <span>({formatNumber(product.numReviews)})</span>
       </div>
+
       <ProductPrice
         isDeal={product.tags.includes("todays-deal")}
         price={product.price}
@@ -80,7 +77,6 @@ const ProductCard = ({
       />
     </div>
   );
-
   const AddButton = () => (
     <div className="w-full text-center">
       <AddToCart
@@ -105,21 +101,29 @@ const ProductCard = ({
   return hideBorder ? (
     <div className="flex flex-col">
       <ProductImage />
-      {!hideDetails && <ProductDetails />}
-      {!hideAddToCart && <AddButton />}
+      {!hideDetails && (
+        <>
+          <div className="p-3 flex-1 text-center">
+            <ProductDetails />
+          </div>
+          {!hideAddToCart && <AddButton />}
+        </>
+      )}
     </div>
   ) : (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col  ">
       <CardHeader className="p-3">
         <ProductImage />
       </CardHeader>
-      <CardContent className="p-3 flex-1 text-center">
-        {!hideDetails && <ProductDetails />}
-      </CardContent>
-      {!hideAddToCart && (
-        <CardFooter className="p-3">
-          <AddButton />
-        </CardFooter>
+      {!hideDetails && (
+        <>
+          <CardContent className="p-3 flex-1  text-center">
+            <ProductDetails />
+          </CardContent>
+          <CardFooter className="p-3">
+            {!hideAddToCart && <AddButton />}
+          </CardFooter>
+        </>
       )}
     </Card>
   );
