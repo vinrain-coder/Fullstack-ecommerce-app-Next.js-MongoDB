c65c8e631cfefa052a68efeea54a5d1d645abb2e
vinrain-coder <vincentombogo57@gmail.com>
2025-03-21 17:34:06 +0000

    Edit images

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
   return (
     <div className="fixed inset-0 flex justify-center items-center">
       <div className="flex flex-col items-center space-y-4">
-        <span className="text-lg font-semibold animate-pulse text-primary">
+        <span className="text-xl font-semibold animate-pulse text-primary">
           ShoePedi is loading...
         </span>
       </div>
