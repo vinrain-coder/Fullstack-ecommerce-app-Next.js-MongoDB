305c51fc442a28e2ca5aab5fc6c5fc5a1a092497
vinrain-coder <vincentombogo57@gmail.com>
2025-03-21 10:06:11 +0000

    update static page

--- app/[locale]/(root)/product/[slug]/page.tsx
+++ app/[locale]/(root)/product/[slug]/page.tsx
@@ -27,22 +27,20 @@ import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
   const slugs = await getAllProductSlugs();
-
-  return slugs.slice(0, 100).map((slug) => ({
-    slug,
-  }));
+  return slugs.slice(0, 100).map((slug) => ({ slug }));
 }
 
 export const revalidate = 3600;
 
-export async function generateMetadata(props: {
-  params: Promise<{ slug: string }>;
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
 }) {
-  // const t = await getTranslations();
-  const params = await props.params;
   const product = await getProductBySlug(params.slug);
+  // const t = await getTranslations();
   if (!product) {
-    return notFound();
+    return { title: "Product Not Found" };
   }
 
   const ogImageUrl = product.images[0];
@@ -79,14 +77,8 @@ export async function generateMetadata(props: {
       images: [ogImageUrl],
     },
     additionalMetaTags: [
-      {
-        property: "product:price:amount",
-        content: product.price.toString(),
-      },
-      {
-        property: "product:price:currency",
-        content: "KES",
-      },
+      { property: "product:price:amount", content: product.price.toString() },
+      { property: "product:price:currency", content: "KES" },
     ],
     jsonLd: {
       "@context": "https://schema.org/",
@@ -94,10 +86,7 @@ export async function generateMetadata(props: {
       name: product.name,
       image: ogImageUrl,
       description: product.description,
-      brand: {
-        "@type": "Brand",
-        name: "ShoePedi",
-      },
+      brand: { "@type": "Brand", name: "ShoePedi" },
       offers: {
         "@type": "Offer",
         url: `${site.url}/product/${product.slug}`,
@@ -108,19 +97,18 @@ export async function generateMetadata(props: {
   };
 }
 
-export default async function ProductDetails(props: {
-  params: Promise<{ slug: string }>;
-  searchParams: Promise<{ page: string; color: string; size: string }>;
+export default async function ProductDetails({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { page?: string; color?: string; size?: string };
 }) {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
-
   const { slug } = params;
   const session = await auth();
   const product = await getProductBySlug(slug);
-  if (!product) {
-    return notFound();
-  }
+  if (!product) return notFound();
+
   const relatedProducts = await getRelatedProductsByCategory({
     category: product.category,
     productId: product._id.toString(),
@@ -135,8 +123,9 @@ export default async function ProductDetails(props: {
         id={product._id.toString()}
         category={product.category}
       />
+
       <section>
-        <div className="grid grid-cols-1 md:grid-cols-5">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-6">
           <div className="col-span-2">
             <ProductGallery
               images={product.images}
@@ -145,7 +134,7 @@ export default async function ProductDetails(props: {
             />
           </div>
 
-          <div className="flex w-full flex-col gap-2 md:p-5 col-span-2">
+          <div className="col-span-2 flex flex-col gap-4 md:p-5">
             <div className="flex flex-col gap-3">
               <p className="p-medium-16 rounded-full bg-grey-500/10 text-grey-500">
                 {t("Product.Brand")} {product.brand} {product.category}
@@ -159,118 +148,103 @@ export default async function ProductDetails(props: {
                 ratingDistribution={product.ratingDistribution}
               />
               <Separator />
-              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
-                <div className="flex gap-3">
-                  <ProductPrice
-                    price={product.price}
-                    listPrice={product.listPrice}
-                    isDeal={product.tags.includes("todays-deal")}
-                    forListing={false}
-                  />
-                </div>
+              <div className="flex gap-3">
+                <ProductPrice
+                  price={product.price}
+                  listPrice={product.listPrice}
+                  isDeal={product.tags.includes("todays-deal")}
+                  forListing={false}
+                />
               </div>
             </div>
-            <div>
-              <SelectVariant
-                product={product}
-                size={searchParams.size || product.sizes[0]}
-                color={searchParams.color || product.colors[0]}
-              />
-            </div>
+
+            <SelectVariant
+              product={product}
+              size={searchParams.size || product.sizes[0]}
+              color={searchParams.color || product.colors[0]}
+            />
+
             <Separator className="my-2" />
-            <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">
-                {t("Product.Description")}:
-              </p>
-              <p className="p-medium-16 lg:p-regular-18">
-                {product.description}
-              </p>
-            </div>
+            <p className="p-bold-20 text-grey-600">
+              {t("Product.Description")}:
+            </p>
+            <p className="p-medium-16 lg:p-regular-18">{product.description}</p>
           </div>
+
           <div>
             <Card>
               <CardContent className="p-4 flex flex-col gap-4">
                 <ProductPrice price={product.price} />
+
                 {product.countInStock > 0 && product.countInStock <= 3 && (
                   <div className="text-destructive font-bold">
-                    Only few left in stock - order soon
+                    Only a few left in stock - order soon
                   </div>
                 )}
+
+                <div
+                  className={
+                    product.countInStock !== 0
+                      ? "text-green-700 text-xl"
+                      : "text-destructive text-xl"
+                  }
+                >
+                  {product.countInStock !== 0
+                    ? t("Product.In Stock")
+                    : t("Product.Out of Stock")}
+                </div>
 
                 {product.countInStock !== 0 ? (
-                  <div className="text-green-700 text-xl">
-                    {t("Product.In Stock")}
+                  <div className="flex flex-col gap-2 items-center">
+                    <AddToCart
+                      item={{
+                        clientId: generateId(),
+                        product: product._id.toString(),
+                        countInStock: product.countInStock,
+                        name: product.name,
+                        slug: product.slug,
+                        category: product.category,
+                        price: round2(product.price),
+                        quantity: 1,
+                        image: product.images[0],
+                        size: searchParams.size || product.sizes[0],
+                        color: searchParams.color || product.colors[0],
+                      }}
+                    />
+                    <OrderViaWhatsApp
+                      productName={product.name}
+                      variant={searchParams.color || product.colors[0]}
+                      size={searchParams.size || product.sizes[0]}
+                      quantity={1}
+                      price={product.price}
+                    />
+                    <WishlistButton
+                      productId={product._id.toString()}
+                      initialWishlist={[]}
+                    />
                   </div>
                 ) : (
-                  <div className="text-destructive text-xl">
-                    {t("Product.Out of Stock")}
-                  </div>
-                )}
-
-                {/* This block will show when the product is in stock */}
-                {product.countInStock !== 0 && (
-                  <div className="flex justify-center items-center">
-                    <div className="flex flex-col gap-2 items-center">
-                      <AddToCart
-                        item={{
-                          clientId: generateId(),
-                          product: product._id.toString(),
-                          countInStock: product.countInStock,
-                          name: product.name,
-                          slug: product.slug,
-                          category: product.category,
-                          price: round2(product.price),
-                          quantity: 1,
-                          image: product.images[0],
-                          size: searchParams.size || product.sizes[0],
-                          color: searchParams.color || product.colors[0],
-                        }}
-                      />
-                      <OrderViaWhatsApp
-                        productName={product.name}
-                        variant={searchParams.color || product.colors[0]}
-                        size={searchParams.size || product.sizes[0]}
-                        quantity={1}
-                        price={product.price}
-                      />
-
-                      <WishlistButton
-                        productId={product._id.toString()}
-                        initialWishlist={[]}
-                      />
-                    </div>
-                  </div>
-                )}
-
-                {product.countInStock === 0 && (
-                  <div className="flex justify-center items-center mt-4">
-                    <SubscribeButton productId={product._id.toString()} />
-                  </div>
+                  <SubscribeButton productId={product._id.toString()} />
                 )}
               </CardContent>
             </Card>
           </div>
         </div>
       </section>
-      <div className="flex flex-col gap-2 my-2">
+
+      <div className="my-2">
         <h3 className="font-semibold">Share this product</h3>
         <ShareProduct slug={product.slug} name={product.name} />
       </div>
-      <section className="mt-10">
-        <h2 className="h2-bold mb-2" id="reviews">
-          {t("Product.Customer Reviews")}
-        </h2>
-        <ReviewList product={product} userId={session?.user.id} />
-      </section>
-      <section className="mt-10">
-        <ProductSlider
-          products={relatedProducts.data}
-          title={t("Product.Best Sellers in", { name: product.category })}
-        />
-      </section>
-      <section>
-        <BrowsingHistoryList className="mt-10" />
-      </section>
+
+      <ReviewList product={product} userId={session?.user.id} />
+
+      <ProductSlider
+        products={relatedProducts.data}
+        title={t("Product.Best Sellers in", { name: product.category })}
+      />
+
+      <BrowsingHistoryList className="mt-10" />
     </div>
   );
 }
