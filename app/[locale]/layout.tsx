88ba0b16cea698563b2168d2179d7f47fdbfd131
vinrain-coder <vincentombogo57@gmail.com>
2025-03-22 21:25:01 +0000

    Add opengraph image and layout metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,13 +19,45 @@ export async function generateMetadata() {
   const {
     site: { slogan, name, description, url },
   } = await getSetting();
+
+  const title = `${name} | ${slogan}`;
+  const imageUrl = `${url}/opengraph-image.jpg`;
+
   return {
     title: {
       template: `%s | ${name}`,
-      default: `${name} | ${slogan}`,
+      default: title,
     },
-    description: description,
+    description,
     metadataBase: new URL(url),
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName: name,
+      images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+          alt: `${name} - ${slogan}`,
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [imageUrl],
+    },
+    alternates: {
+      canonical: url,
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
   };
 }
 
