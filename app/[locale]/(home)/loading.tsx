23f7bbba43c150af73af08bb4f6c4e6222777183
vinrain-coder <vincentombogo57@gmail.com>
2025-03-31 13:08:10 +0000

    Edit home skeleton

--- app/[locale]/(home)/loading.tsx
+++ app/[locale]/(home)/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="space-y-4 p-4">
       {/* Carousel Placeholder */}
-      <div className="w-full h-[350px] md:h-[400px] bg-gray-300 animate-pulse rounded-md"></div>
+      <div className="w-full h-[50px] md:h-[400px] animate-pulse rounded-md"></div>
 
       {/* Category Cards Placeholder */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
